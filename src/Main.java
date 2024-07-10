d073beb5fbf87caf23c19809472c05c07f9c620e
varvaragr <grechishnikova.varvara@gmail.com>
2024-07-10 23:54:31 +0300

    дз

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Author firstBook = new Author("Nikolay", "Gogol");
-        Author secondBook = new Author("Fedor", "Dostoevski");
-        Book one = new Book("Nose", firstBook, 1836);
-        Book two= new Book ("Idiot", secondBook, 1869);
-        System.out.println("Название книги: " + one.getBookName() + ". Автор: " + firstBook.getFirstName() + " " + firstBook.getLastName() + ". Год издания: " + one.getYearOfPublication());
-        System.out.println("Название книги: " + two.getBookName() + ". Автор: " + secondBook.getFirstName() + " " + secondBook.getLastName() + ". Год издания: " + two.getYearOfPublication());
+        Author firstAuthor = new Author("Nikolay", "Gogol");
+        Author secondAuthor = new Author("Fedor", "Dostoevski");
+        Book one = new Book("Nose", firstAuthor, 1836);
+        Book two= new Book ("Idiot", secondAuthor, 1869);
+        System.out.println("Название книги: " + one.getBookName() + ". Автор: " + firstAuthor.getFirstName() + " " + firstAuthor.getLastName() + ". Год издания: " + one.getYearOfPublication());
+        System.out.println("Название книги: " + two.getBookName() + ". Автор: " + secondAuthor.getFirstName() + " " + secondAuthor.getLastName() + ". Год издания: " + two.getYearOfPublication());
         one.setYearOfPublication(1833);
-        System.out.println("Название книги: " + one.getBookName() + ". Автор: " +  firstBook.getFirstName() + " " + firstBook.getLastName() +  ". Год издания: " + one.getYearOfPublication());
+        System.out.println("Название книги: " + one.getBookName() + ". Автор: " +  firstAuthor.getFirstName() + " " + firstAuthor.getLastName() +  ". Год издания (изменённый): " + one.getYearOfPublication());
 
 
 
